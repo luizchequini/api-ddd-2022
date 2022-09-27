5d00c7f4f4052e80bfdf0ec4c0f84fac662b1e1b
Luiz Chequini <luizchequini@gmail.com>
2022-09-26 22:39:32 -0300

    Parte 2 - Api Net 6 = DDD 2022 - Identity, Linq, Json Web token, Tdd, Docs

---
+++ Entities/Entities/Entities/Notifies.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Entities.Entities
+{
+    public class Notifies
+    {
+        public Notifies()
+        {
+            Notificacoes = new List<Notifies>();
+        }
+
+        [NotMapped]
+        public string? NomePropriedade { get; set; }
+        
+        [NotMapped]
+        public string? Mensagem { get; set; }
+
+        [NotMapped]
+        public List<Notifies> Notificacoes { get; set; }
+
+        public bool ValidadrPropriedadeString(string valor, string nomePropriedade)
+        {
+            if(string.IsNullOrWhiteSpace(valor) || string.IsNullOrWhiteSpace(nomePropriedade))
+            {
+                Notificacoes.Add(new Notifies
+                {
+                    Mensagem = "Campo obrigatório",
+                    NomePropriedade = nomePropriedade
+                });
+
+                return false;
+            }
+
+            return true;
+        }
+
+        public bool ValidadrPropriedadeInt(int valor, string nomePropriedade)
+        {
+            if (valor < 1 || string.IsNullOrWhiteSpace(nomePropriedade))
+            {
+                Notificacoes.Add(new Notifies
+                {
+                    Mensagem = "Campo obrigatório",
+                    NomePropriedade = nomePropriedade
+                });
+
+                return false;
+            }
+
+            return true;
+        }
+    }
+}
